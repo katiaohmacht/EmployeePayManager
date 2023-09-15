1db08997e02c62a271b236dd7d9dd16ce502c47a
katiaohmacht <99374417+katiaohmacht@users.noreply.github.com>
2023-09-14 22:57:14 -0400

    Updated table

--- db/migrate/20230607142651_create_users_table.rb
+++ db/migrate/20230607142651_create_users_table.rb
@@ -1,8 +1,13 @@
 class CreateUsersTable < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |entry|
-      entry.string :id
+      entry.string :username
+      entry.string :employee_id
       entry.string :password_hash
+      entry.boolean :admin
+      entry.string :job
+      entry.integer :salary
+      entry.string :address
       entry.string :first_name
       entry.string :last_name
       entry.datetime :created_at
